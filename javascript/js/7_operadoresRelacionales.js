8704b64ef1e3c795c0df8defafea652737b341fe
Carolina Muñoz <cm7283155@gmail.com>
2021-11-19 16:51:35 -0500

    añadiendo archivos

--- javascript/js/7_operadoresRelacionales.js
+++ javascript/js/7_operadoresRelacionales.js
@@ -1,7 +1,8 @@
 "use strict"
 
-console.log("Mayor que...................>")
-console.log("Menor que...................<")
-console.log("Mayor o igual que ...............")
-console.log("")
-console.log("")
+console.log("Mayor que.....................>")
+console.log("Menor que.....................<")
+console.log("Mayor o igual que ............>=")
+console.log("Menor o igual que ............<=")
+console.log("Igual que.....................==")
+console.log("Estrictamente igual...........===")
