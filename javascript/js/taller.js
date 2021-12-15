74dedab984f12635d78ff574143ed864a4fde3ad
Carolina Muñoz <cm7283155@gmail.com>
2021-12-14 22:23:07 -0500

    añadiendo javascript

---
+++ javascript/js/taller.js
@@ -0,0 +1,28 @@
+/* Pasando el texto a mayusculas */
+var ciudad1 = "1_SaLaDoBlAnCo";
+ciudad1 = ciudad1.toUpperCase();
+console.log(ciudad1); 
+
+/* Pasando a minusculas */
+var ciudad2 = "2_pItalITO";
+ciudad2 = ciudad2.toLowerCase();
+console.log(ciudad2);
+
+/* Primer letra en mayusculas y las demas en minuscula */
+var ciudad3 = "neiva_3";
+ciudad3 = ciudad3[0].toUpperCase() + ciudad3.slice(1);
+console.log(ciudad3);
+
+/*Extrayendo partes de la cadena de texto */
+var anyo2 = "2020";
+var auxConsecutivo = anyo2.slice(1,4);
+
+/* contando caracteres */
+var tematica = "programación orientada a objetos";
+console.log("longitud: " + tematica.length);
+
+/* Buscando espacios dobles  */
+var nombres = "Carolina  Muñoz";
+for (i = 0; i
+    
+    /* Reemplazando parte de  */)
