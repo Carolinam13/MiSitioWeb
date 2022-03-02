5cce2f29e8aa0de7903f62508374b5fcfdde73a0
unknown <SENA@PITAPRTNNFSD271.FormacionSena.RED>
2022-03-02 17:35:59 -0500

    add ejercicio

--- js/cotizacion_productos.js
+++ js/cotizacion_productos.js
@@ -3,21 +3,29 @@ let cantidad = "";
 
 function aumentar(){
     if( inicio <= 9) {
-        cantidad = document.getElementById('cantidadproduc').value = ++inicio;
+        /* cantidad = document.getElementById('cantidadProductos').value = ++inicio; */
+        let unidades = document.getElementById('cantidadProductos').innerHTML;
+        unidades++;
+        document.getElementById('cantidadProductos').innerHTML = unidades;
         document.getElementById("total").innerHTML = cantidad;
     }
+    calcularSubTotal();
 }
 
 function disminuir() {
     if(inicio >= 1) {
-        cantidad = document.getElementById('cantidadproduc').value = --inicio;
+        /* cantidad = document.getElementById('cantidadProductos').value = --inicio; */
         document.getElementById("total").innerHTML = cantidad;
     }
+    calcularSubTotal();
 }
 
-function calcularTotal() {
+function calcularSubTotal() {
+    let numeroUnidades = document.getElementById("cantidadProductos").innerHTML;
+
     let valorUnidad = document.getElementById("valorUnitario").innerHTML;
-    let cantidadElementos = document.getElementById("total").innerHTML;
-    let valorCompra = valorUnidad * cantidadElementos;
-    document.getElementById("multiplicar").innerHTML = valorCompra;
+
+    let vlrSubTotal = Number(numeroUnidades) * Number(valorUnidad);
+alert(vlrSubTotal);
+  /*   document.getElementById("").innerHTML = valorCompra; */
 }
