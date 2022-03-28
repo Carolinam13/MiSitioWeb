7b352808a4fd443172179fbf0bdd3b23a2af33c8
Carolina Muñoz <cm7283155@gmail.com>
2022-03-28 16:28:22 -0500

    add ejercicios

---
+++ carolina/js/main_menu.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("main_menu");
+let nav = document.createElement("nav");
+let ul = document.createElement("ul");
+
+nav.appendChild(ul);
+menu.setAttribute("class", "style_menu");
