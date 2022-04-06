c75cc8d6e637528ff4ba7d03965d0e5a2faf8ee2
Carolina Muñoz <cm7283155@gmail.com>
2022-04-06 15:58:07 -0500

    add transform

---
+++ transform/js/transform.js
@@ -0,0 +1,9 @@
+let botonForm = document.getElementById("botonForm");
+let div4 = document.getElementById("div4");
+
+let botonCerrar = document.getElementById("botonCerrar");
+
+botonForm.addEventListener("click", mostrarForm);
+
+botonCerrar.addEventListener("click", ocultarForm);
+
