b8c334a20a67d5d615ed9215c26410885161c2de
Carolina Muñoz <cm7283155@gmail.com>
2022-05-18 12:22:26 -0500

    add

--- ejer javascript/js/homecenter.js
+++ ejer javascript/js/homecenter.js
@@ -1,32 +1,72 @@
-"Use strict"
 let botonForm = document.getElementById("botonForm");
-let div1 = document.getElementById("div1");
+let div4 = document.getElementById("div4");
 
-let botonCerrar = document.getElementById("botonCerrar");
+
+let botonCerrar1 = document.getElementById("botonCerrar1");
 
 botonForm.addEventListener("click", mostrarForm);
 
-botonCerrar.addEventListener("click", ocultarForm);
+botonCerrar1.addEventListener("click", ocultarForm);
 
 function mostrarForm() {
-    div1.classList.add("transform2");
-    div1.classList.remove("transform1");
-    botonCerrar.classList.add("botonCerrarVisible");
-    botonCerrar.classList.remove("botonCerrar");
-
-    let div2 = document.createElement("div")
-    let contenedor = document.getElementById("div2");
-    contenedor.appendChild(div2);
-    contenedor.setAttribute("class", "div2");
-    let desc = document.createElement("label");
-    div2.appendChild(desc);
-    let text = document.createTextNode();
-    desc.appendChild(text);
-    desc.setAttribute("class", "styleDescrip");
+    div4.classList.add("transform5");
+    div4.classList.remove ("transform4");
+    botonCerrar1.classList.add("botonCerrarVisible");
+    botonCerrar1.classList.remove("botonCerrar");
 }
 function ocultarForm(){
-    div1.classList.remove("transform2");
-    div1.classList.add("transform1");
-    botonCerrar.classList.remove("botonCerrarVisible");
-    botonCerrar.classList.add("botonCerrar");
+    div4.classList.remove("transform5");
+    div4.classList.add("transform4");
+    botonCerrar1.classList.remove("botonCerrarVisible");
+    botonCerrar1.classList.add("botonCerrar1");
+
+}
+ 
+
+"use strict"
+
+let botonOpen = document.getElementById("chat-box-disable");
+let botonCerrar = document.getElementById("botonCerrar");
+let botonOpenHeader = document.getElementById("header-movil-open");
+let botonCerrarHeader = document.getElementById("boton-cerrar-header");
+let Header = document.getElementById("header");
+
+botonOpen.addEventListener("click", dezplegarCaja);
+botonCerrar.addEventListener("click", cerrarCaja);
+
+function dezplegarCaja(){
+
+    botonOpen.classList.add("chat-box-disable-in");
+    botonOpen.classList.remove("chat-box-disable-out");
+    document.getElementById("chat-box").classList.add("chat-box-in");
+    document.getElementById("chat-box").classList.remove("chat-box-out");
+
+}
+
+function cerrarCaja(){
+    
+    botonOpen.classList.remove("chat-box-disable-in");
+    botonOpen.classList.add("chat-box-disable-out");
+    document.getElementById("chat-box").classList.remove("chat-box-in");
+    document.getElementById("chat-box").classList.add("chat-box-out");
+};
+
+botonOpenHeader.addEventListener("click", dezplegarCajaHeader);
+botonCerrarHeader.addEventListener("click", cerrarCajaHeader);
+
+function dezplegarCajaHeader(){
+
+    document.getElementById("header-movil").classList.add("contenedor-barra-busqueda-movil");
+
+    document.getElementById("header-movil").classList.remove("contenedor-barra-busqueda-movil-out");
+
+    document.getElementById("header").style.display="none";
+}
+
+function cerrarCajaHeader(){
+    document.getElementById("header-movil").classList.remove("contenedor-barra-busqueda-movil");
+
+    document.getElementById("header-movil").classList.add("contenedor-barra-busqueda-movil-out");
+
+    document.getElementById("header").style.display="flex";
 }
